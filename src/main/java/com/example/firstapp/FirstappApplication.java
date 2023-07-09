3b22045b2cf7bd8b4ec60dc2d9485647bcbe2d86
Lucas Bledsoe <lbledsoe34@gmail.com>
2023-07-09 14:52:21 -0400

    pom.xml

--- src/main/java/com/example/firstapp/FirstappApplication.java
+++ src/main/java/com/example/firstapp/FirstappApplication.java
@@ -1,14 +1,19 @@
 package com.example.firstapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class FirstappApplication {
+public class FirstappApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstappApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO students "
 	}
 
 }
