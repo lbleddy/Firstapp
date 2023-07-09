7e4c52686b82c1bdafaa5ae39c55cafa3108f679
Lucas Bledsoe <lbledsoe34@gmail.com>
2023-07-09 14:08:43 -0400

    here comes the weekend

--- src/main/java/com/example/user/User.java
+++ src/main/java/com/example/user/User.java
@@ -1,2 +1,14 @@
-package com.example.user;public class User {
+package com.example.user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "userdata")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 }
